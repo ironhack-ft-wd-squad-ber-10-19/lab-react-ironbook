194bf2855e06969007f6599a0fde679b3c85beaa
Kara P <quetzlcoatlus@gmail.com>
2020-12-02 17:42:00 +0100

    i3 done, but unchecking has no effect on the list

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -15,25 +15,72 @@ class App extends Component {
 
   state = {
     userList: users,
-    search: ''
+    search: '',
+    student: false,
+    teacher: false,
+  }
+
+  handleStudents = event => {
+    
+    const filteredStudents = this.state.userList.filter(user => {
+      if (event.target.checked) {
+        return user.role === "student"
+      } else {
+        return user
+      }
+    })
+    this.setState({
+      student: event.target.checked,
+      userList: filteredStudents
+    })
+  }
+
+  handleTeachers = event => {
+    
+    const filteredTeachers = this.state.userList.filter(user => {
+      if (event.target.checked) {
+        return user.role === "teacher"
+      } else {
+        return user
+      }
+    })
+    this.setState({
+      teacher: event.target.checked,
+      userList: filteredTeachers
+    })
   }
 
   handleChange = event => {
     // event.preventDefault();
     console.log('rodger rodger')
+    // const { name, value } = event.target
 
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    
     this.setState({
         search: event.target.value
       }, () => this.handleSearch())
     }
-    
+
   handleSearch = () => {
     const {search} = this.state;
 
     const newSearch = users.filter(user => {
-      if (user.lastName.toLowerCase().includes(search.toLowerCase()) || user.firstName.toLowerCase().includes(search.toLowerCase()) ) 
-      {
-        return user.lastName.toLowerCase().includes(search.toLowerCase()) || user.firstName.toLowerCase().includes(search.toLowerCase());
+      if (this.state.teacher){
+        return user.role === "teacher" &&
+         user.lastName.toLowerCase().includes(search.toLowerCase()) 
+        || user.firstName.toLowerCase().includes(search.toLowerCase()) ;
+
+      } else if (this.state.student) {
+        return user.role === "student" && 
+        user.lastName.toLowerCase().includes(search.toLowerCase()) 
+        || user.firstName.toLowerCase().includes(search.toLowerCase());
+        
+      } else {
+        return user.lastName.toLowerCase().includes(search.toLowerCase()) 
+        || user.firstName.toLowerCase().includes(search.toLowerCase());
       }
     })
     this.setState({
@@ -81,16 +128,42 @@ class App extends Component {
         <h1>IronBook</h1>
 
         <div>
-        <label htmlFor="search">
-          {/* Search by First or Last name */}
+          <label htmlFor="search">
+            <input type ="text" name ="search" id= "search" placeholder=" Search by First or Last name.." 
+              value={this.state.search} onChange={this.handleChange} 
+              class="searchBar"
+            />
+          </label>
+          <p>
+  
+            <label htmlFor="student">Student</label>
+            <input
+              type="checkbox"
+              name="student"
+              id="student"
+              checked={this.state.student}
+              onChange={this.handleStudents}
+            // onChange={this.handleCheckboxChange}
+            />
         
-        <input type ="text" name ="search" id= "search" placeholder=" Search by First or Last name.." 
-          value={this.state.search} onChange={this.handleChange} 
-        />
-        </label>
+
+          <label htmlFor="teacher">Teacher</label>
+            <input
+              type="checkbox"
+              name="teacher"
+              id="teacher"
+              checked={this.state.teacher}
+              onChange={this.handleTeachers}
+            // onChange={this.handleCheckboxChange}
+            />
+
+          </p>
+
+            
+
         </div>
 
-        <table >
+        <table>
           <thead>
             <tr>
               <th>First Name</th>
